f2e9613936753a337cf258169af7eda0980dd6cd
Colin Curtin <colin.t.curtin@gmail.com>
2011-01-10 12:32:39 -0800

    Public dir, and remove loop.

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 jokes = File.read('jokes.txt').split("\n\n").map(&:strip).map{|s| s.gsub(/\d+\. /, '')}
 
+set :public, File.dirname(__FILE__) + '/public'
 post '/' do
   @r = Twilio::Response.new
   
-  @r.append(Twilio::Say.new(jokes.at(rand(jokes.size)), :voice => "man", :loop => "10"))
+  @r.append(Twilio::Say.new(jokes.at(rand(jokes.size)), :voice => "man"))
   @r.append(Twilio::Play.new("http://yomomma.heroku.com/102_ya_mama_matt_dike_remix_1.mp3"))
   @r.respond
 end
